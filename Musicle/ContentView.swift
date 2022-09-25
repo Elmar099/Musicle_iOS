1596d1fad4c84c0f3d5ed57b1ec003b4fa231899
Elmar <elmar6999@gmail.com>
2022-09-25 16:58:05 -0700

    Initial Commit

---
+++ Musicle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Musicle
+//
+//  Created by Yusif Imanov on 9/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Musicle/MusicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicleApp.swift
+//  Musicle
+//
+//  Created by Yusif Imanov on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
